0e484efce385f45494ec37a0597f9a941b7c79ab
mkbktaytay <12310513@mail.sustech.edu.cn>
2024-11-25 12:21:40 +0000

    cal_top

--- project/calculate_mode/shift_operation.v
+++ project/calculate_mode/shift_operation.v
@@ -1,9 +1,9 @@
 module shift_operation(
     input signed [7:0] a,    // 输入的 8 位有符号数（算术移位用）
-    input [2:0] b,           // 输入的无符号数，表示移位的位数（范围 0~7）
+    input [7:0] b,           // 输入的无符号数，表示移位的位数（范围 0~7）
     input [3:0] op,          // 选择模式
-    output reg [7:0] result  // 算术左移结果
-    output [7:0] leds,       // 对应的led灯显示
+    output reg [7:0] result // 算术左移结果
+
 );
     always @(*) begin
         case(op)
@@ -15,6 +15,4 @@ module shift_operation(
         endcase
     end
 
-    // 将结果直接映射到 leds
-    assign leds = result;
 endmodule
